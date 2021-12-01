7671179921ebadc1f795fecbbb13cd484139c2e5
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2021-12-01 11:51:24 +0530

    Implement DRY

--- src/main/java/com/parkinglotsystem/vehicles/Vehicle.java
+++ src/main/java/com/parkinglotsystem/vehicles/Vehicle.java
@@ -6,5 +6,15 @@ import com.parkinglotsystem.models.ParkingTicket;
 import java.util.ArrayList;
 
 public interface Vehicle {
-    String parkVehicle(ArrayList<ParkingFloor> floors, ParkingTicket tickets, String registrationNumber, String color);
+    default String parkVehicle(ArrayList<ParkingFloor> floors, ParkingTicket tickets, String registrationNumber, String color) {
+        String ticketId = "";
+        for (int floor = 0; floor < floors.size(); floor++) {
+            ParkingFloor eachFloor = floors.get(floor);
+            ticketId = getTicketIfSlotFree(eachFloor, tickets, registrationNumber, color, floor);
+            if (ticketId.length() != 0) return "Parked vehicle. Ticket ID: " + ticketId;
+        }
+        return "Parking Lot Full";
+    }
+
+    String getTicketIfSlotFree(ParkingFloor eachFloor, ParkingTicket tickets, String registrationNumber, String color, int floor);
 }
